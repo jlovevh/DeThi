bc1a7c360bf120394c161e4dfcd33b34cf249383
MinhTienPro <tranminhtien1509@gmail.com>
2018-06-25 09:15:04 +0700

    commit changed

---
+++ app/src/main/java/com/tvt/dethi/activities/Main2Activity.java
@@ -0,0 +1,15 @@
+package com.tvt.dethi.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.tvt.dethi.R;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
